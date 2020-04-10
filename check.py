463f7dea9493d754f35f8c069de78f76b533e36b
anaqi <alinaqi1992@gmail.com>
2020-04-10 01:56:10 -0400

    url changes, link changed to button, check every 15 mins

--- check.py
+++ check.py
@@ -2,12 +2,14 @@ from helium import *
 from time import sleep
 import json
 import requests
+import time
+import datetime
 
 # -- data -- #
-credentials = json.loads(open("credentials.json"))
+credentials = json.load(open("credentials.json"))
 INSTACART_EMAIL = credentials["INSTACART_EMAIL"]
 INSTACART_PASSWORD = credentials["INSTACART_PASSWORD"]
-MAILGUN_URL = credentials["MAILGUN_URL"]
+MAILGUN_URL = credentials["MAILGUN_DOMAIN"]
 MAILGUN_API_KEY = credentials["MAILGUN_API_KEY"]
 STORE_LIST = credentials["STORE_LIST"]
 INSTACART_BASE_URL = credentials["INSTACART_BASE_URL"]
@@ -16,7 +18,7 @@ NOTIFICATION_EMAIL = credentials["NOTIFICATION_EMAIL"]
 
 # -- login logic -- #
 start_chrome(INSTACART_BASE_URL, headless=True)
-click(Link("Log In"))
+click(Button("Log In"))
 write(INSTACART_EMAIL, into="Email address")
 write(INSTACART_PASSWORD, into="Password")
 click(Button("Log In"))
@@ -28,22 +30,26 @@ def check_delivery_times_for_store(store_name):
     go_to(INSTACART_DELIVERY_URL.format(store_name))
     sleep(2)
     if Text("No delivery times available").exists():
-        return False, "No Delivery times available. Try again later?"
+        #print("No Delivery times available")
+        return False, "No Delivery times available for {}".format(store_name)
+    elif Text("There was a problem loading this page").exists():
+        return False, "There was a problem loading this page"
     else:
+        #print("Delivery times found")
         return (
             True,
-            "Delivery times found for {}! Please check soon :)".format(store_name),
+            "Delivery times found for {}! Please check soon :)".format(store_name)
         )
 
 
 # -- send email -- #
 def send_simple_message(message):
     return requests.post(
-        "https://api.mailgun.net/v3/{}/messages".format(MAILGUN_DOMAIN),
+        "https://api.mailgun.net/v3/{}/messages".format(MAILGUN_URL),
         auth=("api", MAILGUN_API_KEY),
         data={
             "from": "Instacart Delivery Notifier <instacart_notify@{}>".format(
-                MAILGUN_DOMAIN
+                MAILGUN_URL
             ),
             "to": NOTIFICATION_EMAIL,
             "subject": "Instacart Delivery Time Available!!",
@@ -54,12 +60,25 @@ def send_simple_message(message):
 
 # -- check all stores in list and notify -- #
 def main():
-    for store in STORE_LIST:
-        availability, message = check_delivery_times_for_store(store)
-        if availability:
-            send_simple_message(message)
-        else:
-            pass
+    status = False
+
+    while status == False:
+        print("---------------"+str(datetime.datetime.now())+"------------")
+
+        for store in STORE_LIST:
+            availability, message = check_delivery_times_for_store(store)
+            
+            print (message)
+
+            if availability:
+                send_simple_message(message)
+                print(message)
+
+                #status = True 
+            else:
+                pass
+
+        time.sleep(900)
 
 
 if __name__ == "__main__":
